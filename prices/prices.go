3c812dff80346bdf88b1d0a6bd97a942d526253c
Imran Hasan <imran.h94@gmail.com>
2024-02-11 12:36:39 +0500

    added functionality to load data from file

--- prices/prices.go
+++ prices/prices.go
@@ -1,6 +1,10 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate float64
@@ -8,10 +12,35 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Could Not Open File")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err =scanner.Err()
+	if err != nil {
+		fmt.Println("Reading the file content failed")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+}
+
 func (job TaxIncludedPriceJob)Process() {
 	result := make(map[string]float64)
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.1f", price)] = price * (1 + job.TaxRate)
 	}
 
 	fmt.Println(result)
