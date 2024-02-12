ad5b4e43b1fcd74d1bde75b4cdd1e67e617388e6
Imran Hasan <imran.h94@gmail.com>
2024-02-12 12:30:39 +0500

    created an interface to be able to swap filemanagement logic

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"example.com/price-calculator/cmdmanager"
+	// "example.com/price-calculator/cmdmanager"
 	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
@@ -13,9 +13,9 @@ func main() {
 
 	for _, taxRate := range taxRates {
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cmdm := cmdmanager.New()
-		// priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
